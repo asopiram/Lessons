adccb99ade1b7671df426abca3e72ac2b42b8a27
Dmitrii <dmitrii.sharovarov@gmail.com>
2021-10-31 13:32:21 +0300

    UI_lesson1
    Updated 2021/10/31
    Добавил constrains, прописал IBOutlets для loginInput и passwordInput и IBAction для UIScrollView
    Не обработал ошибки касательно отсутствия members в NSLayoutConstraint: addGestureRecognize, contentInset, scrollIndicatorInsets и endEditing не содержатся в NSLayoutConstraint

--- UI_Lesson1/UI_Lesson1/UI_Lesson1/LoginFormController.swift
+++ UI_Lesson1/UI_Lesson1/UI_Lesson1/LoginFormController.swift
@@ -10,20 +10,61 @@ import UIKit
 class LoginFormController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        super.viewDidLoad() // Жест нажатия
+        let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard)) // Присваиваем его UIScrollVIew
+        scrollView?.addGestureRecognizer(hideKeyboardGesture)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var loginInput: UITextField!
+    
+    @IBOutlet weak var scrollView: NSLayoutConstraint!
+    
+    @IBOutlet weak var passwordInput: UITextField!
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        let login = loginInput.text!
+        // Получаем текст-пароль
+        let password = passwordInput.text!
+        // Проверяем, верны ли они
+        if login == "admin" && password == "123456" {
+            print("успешная авторизация")
+            
+        } else {
+            print("неуспешная авторизация")
+            
+        }
     }
-    */
-
+    
+    
+    // Когда клавиатура появляется
+    @objc func keyboardWasShown(notification: Notification) {
+            // Получаем размер клавиатуры
+    let info = notification.userInfo! as NSDictionary
+    let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+    let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
+            // Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
+    self.scrollView?.contentInset = contentInsets
+    scrollView?.scrollIndicatorInsets = contentInsets
+    
+    }
+        //Когда клавиатура исчезает
+    @objc func keyboardWillBeHidden(notification: Notification) { // Устанавливаем отступ внизу UIScrollView, равный 0 let contentInsets = UIEdgeInsets.zero scrollView?.contentInset = contentInsets
+    }
+    
+    override func viewWillAppear(_ animated: Bool) { super.viewWillAppear(animated)// Подписываемся на два уведомления: одно приходит при появлении клавиатуры
+    NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: UIResponder.keyboardWillShowNotification, object: nil)
+            // Второе — когда она пропадает
+    NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) { super.viewWillDisappear(animated)
+    NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func hideKeyboard() {
+        self.scrollView?.endEditing(true)// исчезновение клавиатуры в портретной ориентации
+    }
+    
 }
